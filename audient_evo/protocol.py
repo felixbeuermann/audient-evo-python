7a1cd629d0786e39eaa93dabca06969e682c338e
felixbeuermann191 <felixbeuermann@web.de>
2026-07-04 19:14:52 +0200

    refactor(protocol)/feat: - moved Protocol related Dicts (SAMPLE_RATES, ....) from device to protocol.py - added other Dicts and classes to protocol that are still being tested

--- audient_evo/protocol.py
+++ audient_evo/protocol.py
@@ -43,3 +43,88 @@ class EvoProtocol:
     def mon_addr(field: int, in_ch: int, out_ch: int) -> int:
         """Return monitor matrix address. (final wValue)"""
         return field + (in_ch - 1) * 4 + (out_ch - 1)
+
+class EVO8Addresses:
+    SAMPLE_RATE    = (0x2900, 0x0200)
+    LOOPBACK_LEFT  = (0x0604, 0x3300)
+    LOOPBACK_RIGHT = (0x0605, 0x3300)
+
+    PHANTOM_POWER  = (0x0000, 0x3A00)
+    GAIN           = (0x0100, 0x3A00)
+    MIC_MUTE       = (0x0200, 0x3A00)
+    MIC_MONO       = (0x0300, 0x3A00)
+
+    VOLUME         = (0x0000, 0x3B00)
+    OUTPUT_MUTE    = (0x0100, 0x3B00)
+
+    MONITOR        = (0x0100, 0x3C00)
+    #MONITOR_MUTE
+    #MONITOR_MONO  = (0x0300, 0x3C00)
+
+    def __init__(self, values):
+        # Store the tuple internally
+        self._values = values
+
+    def __call__(self, index):
+        # Allow the instance to be called like a function: instance(0)
+        return self._values[index]
+
+    def __repr__(self):
+        # Optional: Makes debugging/printing cleaner
+        return f"AddressPair{self._values}"
+
+EVO8TOHARDWARE = {
+    "sample_rate":      (0x2900, 0x0200),
+    "loopback_left":    (0x0604, 0x3300),
+    "loopback_right":   (0x0605, 0x3300),
+
+    "phantom_power":    (0x0000, 0x3A00),
+    "gain":             (0x0100, 0x3A00),
+    "mic_mute":         (0x0200, 0x3A00),
+    "mic_mono":         (0x0300, 0x3A00),
+
+    "volume":           (0x0000, 0x3B00),
+    "output_mute":      (0x0100, 0x3B00),
+
+    }
+
+SAMPLE_RATES = {
+    44100: b'\x44\xAC\x00\x00',
+    48000: b'\x80\xBB\x00\x00',
+    88200: b'\x88\x58\x01\x00',
+    96000: b'\x00\x77\x01\x00'
+}
+
+SAMPLE_RATE_INV = {v: k for k, v in SAMPLE_RATES.items()}
+
+LOOPBACK_MAPPINGS = {
+    "PC1+2": (b'\x06', b'\x07'),
+    "PC3+4": (b'\x08', b'\x09'),
+    "LB1+2": (b'\x0a', b'\x0b'),
+    "MM1+2": (b'\x0c', b'\x0d'),
+    "AM1+2": (b'\x0e', b'\x0f')
+}
+
+LOOPBACK_MAPPINGS_INV = {v: k for k, v in LOOPBACK_MAPPINGS.items()}
+
+HARDWARE_TO_CATEGORY = {
+    # Unit 58 (Inputs)
+    (58, 0x00): "phantom",
+    (58, 0x01): "gain",
+    (58, 0x02): "mic_mute",
+    (58, 0x03): "mono",
+
+    # Unit 59 (Outputs)
+    (59, 0x00): "volume",
+    (59, 0x01): "out_mute",
+
+    # Unit 60 (Monitor)
+    (60, 0x01): "monitor",
+
+    # Unit 2 (sample rate)
+    (2, 0x29): "sample_rate",
+
+    # Unit 51 (loopback)
+    (51, 0x0604): "loopback_left",
+    (51, 0x0605): "loopback_right"
+}
